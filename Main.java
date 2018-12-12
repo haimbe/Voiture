8cc7263bba709f522bf602938d4e93aae0e912e4
Haim <compta@ellecosmetique.com~cd ~cd mon_projetqqgit config --global user.email compta@ellecosmetique.com>
2018-12-12 15:22:59 +0100

    changement du fichier adresse main et Scanner

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    		Voiture voiture = new Voiture();
+    		Scanner sc = new Scanner(System.in);
+    		System.out.println("Veuillez saisir le nombre de roue de votre voiture :");
+    		int str = sc.nextline();
+    		voiture(str);
+	    }
+}
