c099208afc03b28de22d28c96caf625c5a92562a
Haim <compta@ellecosmetique.com~cd ~cd mon_projetqqgit config --global user.email compta@ellecosmetique.com>
2018-12-14 12:33:20 +0100

    changement suite au retour d'Alexandre

--- Voiture.java
+++ Voiture.java
@@ -1,20 +1,21 @@
-Public class Voiture{
+public class Voiture{
     int roue;
 
     /* initialisation*/
-    Public Voiture(){
+    public Voiture(){
+
         roue = 0;
     }
 
     /*constructeur avec parametres*/
 
-    Public Voiture(int roue)
+    public Voiture(int roue)
         throws NombreRoueException, NombreRoue2Exception {
             if (roue <1)
                 throw new NombreRoueException(roue);
             if (roue>4)
                 throw new NombreRoue2Exception(roue);
-            elseif (roue > 1) {
+            else if (roue > 1) {
             System.out.println("Ma voiture possède "+ roue + " roues");}
         else{
             System.out.println ("Ma voiture possède " + roue + " roue");
